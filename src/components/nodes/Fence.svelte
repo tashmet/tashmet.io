ab1919b39714d073bb32ae79d5d9fba46aee11a0
Björn Andersson <bjorn.olof.andersson@gmail.com>
2024-03-18 15:36:31 +0100

    added fence node with hightlight.js

---
+++ src/components/nodes/Fence.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import hljs from 'highlight.js';
+
+	export let language: string;
+	export let content: string;
+
+  $: html = hljs.highlight(content.trim(), { language }).value;
+</script>
+
+<pre class="text-white p-4 my-4 rounded-md text-sm overflow-x-auto" style="background: var(--code-bg)">
+  <code class="language-typescript">{@html html}</code>
+</pre>
---
+++ src/components/nodes/index.svelte
@@ -0,0 +1,3 @@
+<script context="module">
+  export { default as Fence } from './Fence.svelte';
+</script>
