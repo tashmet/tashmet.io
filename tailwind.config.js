a578cb02c845d12a377f1b943eb810f6fd520c25
Björn Andersson <bjorn.olof.andersson@gmail.com>
2024-03-18 15:37:28 +0100

    set up fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        active: '#1faec8',
+        'active-light': '#f1fbfb',
+      },
+    },
   },
   plugins: [],
 }
