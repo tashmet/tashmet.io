fa837cfb52b2a6dea7a20f1814ddf398753cba89
Björn Andersson <bjorn.olof.andersson@gmail.com>
2024-03-25 17:49:30 +0100

    added file tag

---
+++ src/components/tags/File.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div class="file flex flex-col">
+  <div class="file-container flex bg-[#4e7477]">
+    <div class="flex text-xs p-4 pb-2 rounded-t-md mt-2 shadow-xl" style="background: var(--code-bg); color: var(--code-title); border-color: var(--code-title)">
+      <span class="material-symbols-outlined -mt-1 mr-2">
+        draft
+      </span>
+      {title}
+    </div>
+  </div>
+
+  <slot></slot>
+</div>
+
+<style lang="postcss">
+  :global(.file pre) {
+    margin-top: 0;
+    margin-bottom: 0;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+  :global(.editor .file-container) {
+    @apply rounded-t-none
+  }
+  :global(.file-container) {
+    @apply rounded-t-lg
+  }
+</style>
--- src/components/tags/index.svelte
+++ src/components/tags/index.svelte
@@ -1,3 +1,4 @@
 <script context="module">
   export { default as Hint } from './Hint.svelte';
+  export { default as File } from './File.svelte';
 </script>
